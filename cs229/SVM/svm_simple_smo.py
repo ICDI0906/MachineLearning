84e086531f3aa139545af6a15051d5c156713e8a
ICDI0906 <18217060267@163.com>
2018-10-06 19:42:18 +0800

    用smo 来求解svm

---
+++ cs229/SVM/svm_simple_smo.py
@@ -0,0 +1,4 @@
+# @Time : 2018/10/6 下午7:30 
+# @Author : Kaishun Zhang 
+# @File : svm_simple_smo.py 
+# @Function:
---
+++ cs229/plotimg/labelUse.py
@@ -0,0 +1,4 @@
+# @Time : 2018/10/6 下午7:09 
+# @Author : Kaishun Zhang 
+# @File : labelUse.py 
+# @Function:
