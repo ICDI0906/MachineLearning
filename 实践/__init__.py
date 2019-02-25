b407ea53b50ce4f1f87bd215f0420b564b00052e
ICDI0906 <18217060267@163.com>
2019-02-25 23:07:21 +0800

    调整仓库布局

---
+++ 实践/__init__.py
@@ -0,0 +1,8 @@
+# @Time : 2019/2/25 10:59 PM 
+# @Author : Kaishun Zhang 
+# @File : __init__.py.py 
+# @Function:
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Dense, Activation
+from keras.models import Model, Sequential
