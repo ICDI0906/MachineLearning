fce55dfdfd8d9bd5322372f019ab8d1af7c47bad
ICDI0906 <18217060267@163.com>
2019-06-12 13:00:06 +0800

    attention view

--- 实践/papers/A Neural Attention Model for Urban Air Quality Inference: Learning the Weights of Monitoring Stations/Main.py
+++ 实践/papers/A Neural Attention Model for Urban Air Quality Inference: Learning the Weights of Monitoring Stations/Main.py
@@ -49,8 +49,8 @@ def Main():
 
     fc1_mut = tf.nn.relu(tf.matmul(source_dist_angle,fc1_w) + fc1_b)
     fc1_mut = tf.nn.dropout(fc1_mut,keep_prob = 0.9)
-    fc1_mut = tf.reshape(tf.tile(fc1_mut,[1,BATCH_SIZE]),(18,fc1_unit,BATCH_SIZE))
-    fc1_mut = tf.transpose(tf.transpose(fc1_mut,[2,1,0]),[0,2,1]) #(batch_size,18,fc1_unit)
+    fc1_mut = tf.reshape(tf.tile(fc1_mut,[1,BATCH_SIZE]),(18,BATCH_SIZE,fc1_unit))
+    fc1_mut = tf.transpose(fc1_mut,[1,0,2]) #(batch_size,18,fc1_unit)
 
     # two layer lstm
     def lstm_cell():
@@ -65,8 +65,8 @@ def Main():
     # concat lstm layer and fc layer
     lstm_h = states[1][1]
 
-    lstm_h = tf.reshape(tf.tile(lstm_h,[1,18]),(BATCH_SIZE,HIDDEN_SIZE,18))
-    lstm_h = tf.transpose(lstm_h,[0,2,1]) #(batch_size,18,fc1_unit)
+    lstm_h = tf.reshape(tf.tile(lstm_h,[1,18]),(BATCH_SIZE,18,HIDDEN_SIZE))
+    # wrong write lstm_h = tf.transpose(lstm_h,[0,2,1]) #(batch_size,18,fc1_unit)
 
     fc1_lstm = tf.concat([fc1_mut,lstm_h],axis = -1)
     # two fc layer
@@ -94,7 +94,7 @@ def Main():
         fc2_b2_l = tf.Variable(initial_value = tf.random.uniform(shape = (fc2_unit,), minval = -0.1, maxval = 0.1))
 
     # two fc layer
-    fc1_lstm_l = tf.reshape(states[1][1], (-1,HIDDEN_SIZE))
+    fc1_lstm_l = tf.reshape(states_l[1][1], (-1,HIDDEN_SIZE))
     fc2_mut1_l = tf.nn.relu(tf.matmul(fc1_lstm_l, fc2_w1_l) + fc2_b1_l)
     fc2_mut1_l = tf.nn.dropout(fc2_mut1_l, keep_prob = 0.9)
 
@@ -110,6 +110,7 @@ def Main():
         atten_b2 = tf.Variable(initial_value = tf.random_uniform(shape = (1,), minval = -0.1, maxval = 0.1))
 
     atten_x = tf.tile(fc2_mut2_l,[1,18,1])
+
     atten_x = tf.concat([fc2_mut2,atten_x],axis = -1)
     atten_x = tf.reshape(atten_x,(-1,atten_x.shape[2]))
 
@@ -118,11 +119,14 @@ def Main():
     atten_mut2 = tf.matmul(atten_mut1,atten_w2) + atten_b2
     atten_mut2 = tf.nn.dropout(atten_mut2,keep_prob = 0.9)
     atten = tf.reshape(atten_mut2,(BATCH_SIZE,-1))
-    print(atten.shape)
+    # print(atten.shape)
     a_atten = tf.exp(atten) / tf.reshape(tf.tile(tf.reduce_sum(tf.exp(atten),axis = 1),[18]),(BATCH_SIZE,18))
 
-    a_sum = tf.reshape(tf.tile(a_atten,[1,fc2_unit]),(BATCH_SIZE,18,fc2_unit))
+    a_sum = tf.reshape(tf.tile(a_atten,[1,fc2_unit]),(BATCH_SIZE,fc2_unit,18))
+    a_sum = tf.transpose(a_sum,[0,2,1])
+
     a_sum = tf.multiply(a_sum,fc2_mut2)
+
     a_sum = tf.reduce_sum(a_sum, axis = 1)
     fc2_mut2_l = tf.squeeze(fc2_mut2_l)
 
@@ -153,6 +157,7 @@ def Main():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         # random 10 times
+        ans = 0;
         for i in range(10):
             val_error = np.inf
             list_s = list(range(36))
@@ -174,7 +179,7 @@ def Main():
             dist_angle = mergeDistAngle(target_station, source_station)
             dist_angle = dist_angle.reshape(dist_angle.shape[0], dist_angle.shape[1])
 
-            ans = 0;ans_tmp = 0
+            ans_tmp = 0
             for j in range(10):
                 for k in range(n // batch_size + 1):
                     source_train,target_train_mete,target_train_aqi = dp.get_batch_data(batch_size)
@@ -185,9 +190,10 @@ def Main():
                     y_ = np.squeeze(scaler.inverse_transform(result[1]))
                     y_true = np.squeeze(scaler.inverse_transform(target_train_aqi))
 
-                    logger.info('iterator :' + str(j * n + k * batch_size) + 'train loss is {:.4f}'.format(np.sqrt(mean_squared_error(y_true, y_))))
-
+                    # logger.info('is equal {}'.format(result[2]))
                     if k % 50 == 0:
+                        logger.info('iterator : ' + str(j * n + k * batch_size) + '  train loss is {:.4f}'.format(
+                            np.sqrt(mean_squared_error(y_true, y_))))
                         source_val,target_val_mete,target_val_aqi = dp.validation()
                         result = sess.run(y_hat,
                                          feed_dict = {source_input: source_val, target_input_mete: target_val_mete,
@@ -196,33 +202,40 @@ def Main():
                         y_ = np.squeeze(scaler.inverse_transform(result))
                         y_true = np.squeeze(scaler.inverse_transform(target_val_aqi))
                         error = np.sqrt(mean_squared_error(y_true, y_))
-                        logger.info('iterator :' + str(j) + 'val loss is {:.4f}'.format(np.sqrt(mean_squared_error(y_true, y_))))
+                        logger.info('iterator : ' + str(j * n + k * batch_size) + '  val loss is {:.4f}'.format(np.sqrt(mean_squared_error(y_true, y_))))
                         if val_error > error:
                             val_error = error
                             saver.save(sess, 'model/my-model', global_step = j * n + k * batch_size)
                             # test
                             source_test, target_test_mete, target_test_aqi = dp.test()
-                            dist_angle_test = mergeDistAngle(target_station, source_station)
-                            dist_angle_test = dist_angle_test.reshape(dist_angle_test.shape[0], dist_angle_test.shape[1])
 
-                            result = sess.run(y_hat,
+                            result = sess.run([y_hat,a_atten],
                                               feed_dict = {source_input: source_test, target_input_mete: target_test_mete,
-                                                           source_dist_angle: dist_angle_test,BATCH_SIZE:877})
+                                                           source_dist_angle: dist_angle,BATCH_SIZE:877})
 
-                            y_ = np.squeeze(scaler.inverse_transform(result))
+                            y_ = np.squeeze(scaler.inverse_transform(result[0]))
                             y_true = np.squeeze(scaler.inverse_transform(target_test_aqi))
                             ans_tmp = np.sqrt(mean_squared_error(y_true, y_))
-
+                            pd.DataFrame(result[1]).to_csv('attention' + str(i) + '.csv')
                             logger.info('station {} test loss is {:.4f}'.format(target_station, np.sqrt(mean_squared_error(y_true, y_))))
             ans += ans_tmp
-        logger.info('average rmse of random 10 {:.4f}'.format(ans / 10))
+        logger.info('average rmse of random 10 is : {:.4f}'.format(ans / 10))
 
 
 if __name__ == '__main__':
     Main()
-    # arr = np.array([[1,2],[3,4]])
-    #
+    # arr = np.array([[1,2,3],[4,5,6]])
+    # arr = arr.reshape((2,1,3))
     # # print(np.repeat(arr,repeats = 2,axis = 1))
-    # repeat = np.repeat(arr, repeats = 4, axis = 1).reshape(2,2,4).transpose([0,2,1])
+    # repeat = np.repeat(arr, repeats = 4, axis = 1)
     # print(repeat)
     # print(repeat[:,0,:],repeat[:,1,:])
+
+    # lstm_h = tf.constant([[1,2,3],[4,5,6]])
+    # lstm_h = tf.reshape(tf.tile(lstm_h, [1, 18]), (2, 18, 3))
+    # # lstm_h = tf.transpose(lstm_h, [0, 2, 1])  # (batch_size,18,fc1_unit)
+    #
+    # equal = tf.equal(lstm_h[:, 0, :], lstm_h[:, 1, :])
+    # with tf.Session() as sess:
+    #     result = sess.run(lstm_h)
+    #     print(result[:,0,:],result[:,1,:],result.shape)
